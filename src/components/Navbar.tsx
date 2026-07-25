8b0fe17781cfd062e01f3c94b62185145ea4b126
mo3stx8 <mo3stx7@gmail.com>
2026-07-25 03:00:49 +0300

    #

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -48,7 +48,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 lg:h-20">
           <a href={prefix} className="flex items-center gap-2">
-            <img src={`${basePath}/logo.png`} alt="SiliconCove" className="h-14 lg:h-16 w-auto" />
+            <img src={`${basePath}/logo-old.png`} alt="SiliconCove" className="h-14 lg:h-16 w-auto" />
           </a>
 
           <div className="hidden lg:flex items-center gap-1">
